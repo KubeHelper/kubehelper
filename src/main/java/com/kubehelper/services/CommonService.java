ee24db07dbbef7241f96cf898d13e4ce00df7c0f
JDev <webdevoleg@gmail.com>
2020-11-19 15:15:13 +0100

    add GPLv3 text licenses

--- src/main/java/com/kubehelper/services/CommonService.java
+++ src/main/java/com/kubehelper/services/CommonService.java
@@ -1,3 +1,20 @@
+/*
+Kube Helper
+Copyright (C) 2021 JDev
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 package com.kubehelper.services;
 
 import io.kubernetes.client.openapi.ApiException;
